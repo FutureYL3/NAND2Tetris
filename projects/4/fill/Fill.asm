514d386163babe7e2aa404f3b8fd7bdb56fd6ab5
Xianglin Zeng <Future_YL@outlook.com>
2024-09-01 22:10:27 +0800

    完成proj4并使用本地CPU模拟器通过测试

--- projects/4/fill/Fill.asm
+++ projects/4/fill/Fill.asm
@@ -8,4 +8,99 @@
 // i.e. writes "black" in every pixel. When no key is pressed, 
 // the screen should be cleared.
 
-//// Replace this comment with your code.
+// pseudocode
+// while (true) {
+//   key = RAM[KBD]
+//   if (key != 0) {
+//     for (i = 0; i < 8192; i++) {
+//       addr = SCREEN + i
+//       RAM[addr] = -1
+//     } 
+//   } else {
+//     for (i = 0; i < 8192; i++) {
+//       addr = SCREEN + i
+//       RAM[addr] = 0
+//     } 
+//   }
+// }
+
+  @8192
+  D = A
+  @num
+  M = D // num = 8192
+
+(MAIN_LOOP)
+  @KBD
+  D = M // D = RAM[KBD]
+
+  @NOT_PRESSED
+  D;JEQ
+
+  @PRESSED
+  0;JMP
+
+  (NOT_PRESSED)
+    @i
+    M = 0 // i = 0
+    
+    (INNER_LOOP1)
+      @num
+      D = M
+      @i
+      D = M - D // D = i - num
+
+      @FINISH1
+      D;JGE // if i - num >= 0, then break
+
+      @i
+      D = M
+      @SCREEN
+      A = A + D // A = SCREEN + i
+
+      M = 0 // RAM[SCREEN + i] = 0
+
+      @i
+      M = M + 1 // i++
+
+      @INNER_LOOP1
+      0;JMP
+      
+    (FINISH1)
+      @MAIN_LOOP
+      0;JMP
+
+  (PRESSED)
+    @i
+    M = 0 // i = 0
+    
+    (INNER_LOOP2)
+      @num
+      D = M
+      @i
+      D = M - D // D = i - num
+
+      @FINISH2
+      D;JGE // if i - num >= 0, then break
+
+      @i
+      D = M
+      @SCREEN
+      A = A + D // A = SCREEN + i
+
+      M = -1 // RAM[SCREEN + i] = -1
+
+      @i
+      M = M + 1 // i++
+
+      @INNER_LOOP2
+      0;JMP
+      
+    (FINISH2)
+      @MAIN_LOOP
+      0;JMP
+
+
+
+
+
+  
--- projects/4/mult/Mult.asm
+++ projects/4/mult/Mult.asm
@@ -7,4 +7,138 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 // The algorithm is based on repetitive addition.
 
-//// Replace this comment with your code.
+// pseudocode
+// sum = 0
+// if (RAM[0] == 0) {
+//   // do nothing
+// }
+// if (RAM[0] > 0) {
+//   for (i = RAM[0]; i > 0; i--) {
+//     sum = sum + RAM[1]
+//   }
+// } else if (RAM[1] < 0) {
+//   for (i = RAM[0]; i < 0; i++) {
+//     sum = sum - RAM[1] 
+//   }   
+// } else {
+//   for (i = RAM[1]; i > 0; i--) {
+//     sum = sum + RAM[0] 
+//   } 
+// }
+
+  @sum
+  M = 0
+
+  @R0
+  D = M
+
+  @ZERO
+  D;JEQ // special case
+
+  @POSITIVE
+  D;JGT // First if branch
+
+  @R1
+  D = M
+
+  @BOTH_NEGATIVE
+  D;JLT // Second if branch
+
+  @ONLY_R0_NEGATIVE
+  0;JMP // Third if branch
+
+(ZERO)
+  @FINAL_ZERO
+  0;JMP
+
+
+(POSITIVE)
+  @R0
+  D = M
+  @i
+  M = D // i = RAM[0]
+  
+  (LOOP_POS)
+    @i
+    D = M
+    @FINAL_POS
+    D;JLE // i <= 0, then loop ends
+
+    @R1
+    D = M
+    @sum
+    M = M + D // sum = sum + RAM[1]
+
+    @i
+    M = M - 1 // i--
+
+    @LOOP_POS
+    0;JMP // goto LOOP
+
+  @FINAL_POS
+  0;JMP
+
+(BOTH_NEGATIVE)
+  @R0
+  D = M
+  @i
+  M = D // i = RAM[0]
+  
+  (LOOP_NEG)
+    @i
+    D = M
+    @FINAL_NEG
+    D;JGE // i >= 0, then loop ends
+
+    @R1
+    D = M
+    @sum
+    M = M - D // sum = sum - RAM[1]
+
+    @i
+    M = M + 1 // i++
+
+    @LOOP_NEG
+    0;JMP // goto LOOP
+
+  @FINAL_NEG
+  0;JMP
+
+(ONLY_R0_NEGATIVE)
+  @R1
+  D = M
+  @i
+  M = D // i = RAM[1]
+  
+  (LOOP_ONLY)
+    @i
+    D = M
+    @FINAL_ONLY
+    D;JLE // i <= 0, then loop ends
+
+    @R0
+    D = M
+    @sum
+    M = M + D // sum = sum + RAM[0]
+
+    @i
+    M = M - 1 // i--
+
+    @LOOP_ONLY
+    0;JMP // goto LOOP
+
+  @FINAL_ONLY
+  0;JMP
+
+(FINAL_NEG)
+(FINAL_POS)
+(FINAL_ONLY)
+(FINAL_ZERO)
+  @sum
+  D = M
+  @R2
+  M = D // RAM[2] = sum
+
+(END)
+  @END
+  0;JMP
